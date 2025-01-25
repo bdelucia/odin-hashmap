2cc832399e984c2bc379fd8905ad8bdb06bb00ae
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-25 14:13:13 -0700

    hash factory

---
+++ hashMap.js
@@ -0,0 +1,16 @@
+function HashMap() {
+  let loadFactor = 0.75;
+  let capacity = 16;
+
+  return {
+    hash(key) {
+      let hashCode = 0;
+
+      const primeNumber = 31;
+      for (let i = 0; i < key.length; i++) {
+        hashCode += primeNumber * hashCode + key.charCodeAt(i);
+      }
+      return hashCode;
+    },
+  };
+}
