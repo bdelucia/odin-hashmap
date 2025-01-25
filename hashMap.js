d4563bf3e6c26cad3b87159049b583bfe608fe2f
Robert DeLucia Jr. <bdelucia@asu.edu>
2025-01-25 14:32:19 -0700

    methods

--- hashMap.js
+++ hashMap.js
@@ -1,6 +1,7 @@
 function HashMap() {
   let loadFactor = 0.75;
   let capacity = 16;
+  let buckets = [];
 
   return {
     hash(key) {
@@ -11,6 +12,79 @@ function HashMap() {
         hashCode += primeNumber * hashCode + key.charCodeAt(i);
       }
       return hashCode;
+    },
+    set(key, value) {
+      let hashCode = this.hash(key);
+      let index = hashCode % capacity;
+
+      if (!this.buckets) {
+        this.buckets = [];
+      }
+
+      if (!this.buckets[index]) {
+        this.buckets[index] = [];
+      }
+
+      this.buckets[index] = { key: key, value: value };
+    },
+    get(key) {
+      let hashCode = this.hash(key);
+      let index = hashCode % capacity;
+
+      if (!this.buckets) {
+        return null;
+      }
+
+      return this.buckets[index] ? this.buckets[index].value : null;
+    },
+    remove(key) {
+      let hashCode = this.hash(key);
+      let index = hashCode % capacity;
+
+      if (!this.buckets) {
+        return null;
+      }
+
+      this.buckets[index] = null;
+    },
+    length() {
+      let count = 0;
+      for (let i = 0; i < this.buckets.length; i++) {
+        if (this.buckets[i]) {
+          count++;
+        }
+      }
+      return count;
+    },
+    clear() {
+      this.buckets = [];
+    },
+    keys() {
+      let keys = [];
+      for (let i = 0; i < this.buckets.length; i++) {
+        if (this.buckets[i]) {
+          keys.push(this.buckets[i].key);
+        }
+      }
+      return keys;
+    },
+    values() {
+      let values = [];
+      for (let i = 0; i < this.buckets.length; i++) {
+        if (this.buckets[i]) {
+          values.push(this.buckets[i].value);
+        }
+      }
+      return values;
+    },
+    entries() {
+      let entries = [];
+      for (let i = 0; i < this.buckets.length; i++) {
+        if (this.buckets[i]) {
+          entries.push([this.buckets[i].key, this.buckets[i].value]);
+        }
+      }
+      return entries;
     },
   };
 }
